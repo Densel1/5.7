9c8463f912347766fa8da19349af031aa9c75677
Igor Kastyrkin <kastyrkinIV@ya.ru>
2023-08-05 16:12:02 +0300

    initial commit

---
+++ 1/1/1.cpp
@@ -0,0 +1,46 @@
+﻿// 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+
+void myfunc(std::vector<int> &v)
+{
+
+	std::sort(v.begin(), v.end());
+
+	auto st = std::unique(v.begin(), v.end());
+
+	v.erase(st, v.end());
+}
+
+void print_vec(const std::vector<int> &mp)
+{
+	for (auto it = mp.begin(); it != mp.end(); it++)
+	{
+		if (it != mp.begin()) std::cout << ", ";
+		std::cout << *it;
+	}
+}
+
+int main()
+{
+
+	std::vector<int> mp = { 1,2,3,4,5,1,2,4,6 };
+
+	std::cout << "[IN]: ";
+
+	print_vec(mp);
+
+	myfunc(mp);
+
+	std::cout << std::endl << "[OUT]: ";
+
+	print_vec(mp);
+
+
+
+}
---
+++ 2/2/2.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <list>
+
+
+
+
+template<typename T>
+void print_container(T& container)
+{
+    for (auto it = container.begin(); it != container.end(); it++)
+    {
+        if (it != container.begin()) std::cout << ", ";
+        std::cout << *it;
+    }
+    std::cout << std::endl;
+}
+
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); // four one three two. помните почему такой порядок? :)
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); // one, two, three, four
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); // one, two, three, four
+    
+}
